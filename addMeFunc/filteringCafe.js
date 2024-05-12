cb5c869db7003fb61b5d0d7549c0e3ee9e692dc2
aziz <aazizsigar@gmail.com>
2024-05-12 07:59:57 +0200

    s

--- addMeFunc/filteringCafe.js
+++ addMeFunc/filteringCafe.js
@@ -1,3 +1,3 @@
 const ages = [25, 32, 40, 18, 27];
-const adults = ages.filter(age => age >= 18);
+const adults = ages.filter(age => age = 18);
 console.log(adults); // [25, 32, 40, 27]
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("äsdaaa")
+const products = [
+    { name: "Product 1", price: 10, quantity: 2, shipping: true },
+    { name: "Product 2", price: 15, quantity: 3, shipping: false },
+    { name: "Product 3", price: 20, quantity: 1, shipping: true },
+  ];
+  
+  const shippingCost = 10;
+  const discountThreshold = 20;
+  const discountRate = 0.1; // 10% discount
+  
+  const calculateOrderTotal = (products) => {
+    const { totalPrice, shippingTotal } = products.reduce(
+      (acc, product) => {
+        //declare the objects
+let price=product.price
+let quantity=product.quantity
+        const productPrice = price*quantity
+        acc.totalPrice += productPrice;
+  
+        if (product.shipping) {
+          acc.shippingTotal += shippingCost;
+          if (shippingCost > discountThreshold) {
+            productPrice -= shippingCost * discountRate;
+          }
+        }
+  
+        acc.total += productPrice;
+        return acc;
+      },
+
+      { totalPrice: 0, shippingTotal: 0, total: 0 } // Initial accumulator
+    
+      );
+  
+    const averagePrice = totalPrice / products.length;
+  
+    console.log("Average Price:", averagePrice.toFixed(2));
+    return totalPrice;
+  };
+  
+  const orderTotal = calculateOrderTotal(products);
+  console.log("Order Total:", orderTotal);
+  
